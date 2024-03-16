11aafaaabee1515342dad9a295948cbb59ea0aaf
1alex2lee <lal19@ic.ac.uk>
2024-03-16 22:36:18 +0000

    added google auth

--- ecohaul4/src/App.tsx
+++ ecohaul4/src/App.tsx
@@ -6,6 +6,7 @@ import {
     useWalletModal,
 } from '@vechain/dapp-kit-react';
 import { useEffect, useState } from 'react';
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 
 function App() {
     const { account } = useWallet();
@@ -31,12 +32,23 @@ function App() {
     }, [account, onConnectionStatusChange]);
 
     return (
-        <div className="container" style={{ backgroundColor: 'white', padding: '20px', boxSizing: 'border-box', maxWidth: '100%', margin: '0 auto' }}>
-            {/* Use the imported image */}
-            <img src={logo} alt="Logo" style={{ maxWidth: '50px', maxHeight: '50px' }}/> 
-            <h2 style={{ fontFamily: "'San Francisco', 'Helvetica Neue', Helvetica, Arial, sans-serif", marginBottom: '20px'}}>EcoHaul</h2> {/* Change font family here */}
-            <WalletButton />
-        </div>
+        <GoogleOAuthProvider clientId="63230301863-slr123re4n3tlomcj22v2m8qudmrkkr4.apps.googleusercontent.com">
+            <div className="container" style={{ backgroundColor: 'white', padding: '20px', boxSizing: 'border-box', maxWidth: '100%', margin: '0 auto' }}>
+                {/* Use the imported image */}
+                <img src={logo} alt="Logo" style={{ maxWidth: '50px', maxHeight: '50px' }}/> 
+                <h2 style={{ fontFamily: "'San Francisco', 'Helvetica Neue', Helvetica, Arial, sans-serif", marginBottom: '20px'}}>EcoHaul</h2> {/* Change font family here */}
+                <WalletButton />
+                <div className="label">Sign into your eBay account with Google:</div>
+                <GoogleLogin
+                    onSuccess={credentialResponse => {
+                        console.log(credentialResponse);
+                    }}
+                    onError={() => {
+                        console.log('Login Failed');
+                    }}
+                />
+            </div>
+        </GoogleOAuthProvider>
     );
 }
 
